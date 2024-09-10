73be2a774672b952595d5245b99f01cfb02247f7
greendra <greendra8@hotmail.co.uk>
2024-09-10 13:19:04 +0100

    refactor: Update Supabase environment variables handling in server hooks

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: '0.0.0.0',
-		port: 5173 // or any other port you prefer
-	}
+export default defineConfig(() => {
+	return {
+		plugins: [sveltekit()],
+		server: {
+			host: '0.0.0.0',
+			port: 5173 // or any other port you prefer
+		},
+		define: {
+			__APP_ENV__: process.env.VITE_VERCEL_ENV,
+		},
+	};
 });
